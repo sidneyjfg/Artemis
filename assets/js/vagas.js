a22e6f0aea8b9e5d4b9a0d49d6650203e9eb14a7
SidneyJunio <sidneyjunio191@gmail.com>
2023-10-24 09:14:10 -0300

    Add Premium section

--- assets/js/vagas.js
+++ assets/js/vagas.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Quando um link das categorias é clicado
-    $('.vagas-categories a').on('click', function(e) {
+    $('.vagas-categories a').on('click', function (e) {
         e.preventDefault();  // Previne o comportamento padrão do link
 
         // Pega a categoria do link que foi clicado
         let selectedCategory = $(this).data('category');
 
-        if(selectedCategory === "all") {
+        if (selectedCategory === "all") {
             // Se a categoria selecionada for "all", mostre todas as vagas
             $('.job-offer').show();
         } else {
@@ -16,5 +16,14 @@ $(document).ready(function() {
             // Mostra as vagas que correspondem à categoria selecionada
             $('.job-offer[data-category="' + selectedCategory + '"]').show();
         }
+    });
+});
+$(document).ready(function () {
+    $(".vagas-categories a").click(function () {
+        // Remove a classe 'category-active' de todos os links
+        $(".vagas-categories a").removeClass("category-active");
+
+        // Adiciona a classe 'category-active' no link que foi clicado
+        $(this).addClass("category-active");
     });
 });
